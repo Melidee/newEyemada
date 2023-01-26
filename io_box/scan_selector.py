68e906f9b8942fc2510127c1afef7f12c8e639b9
Melidee <86878748+Melidee@users.noreply.github.com>
2023-01-25 22:37:29 -0600

    finish GUI

--- io_box/scan_selector.py
+++ io_box/scan_selector.py
@@ -3,21 +3,39 @@ from tkinter import StringVar, ttk
 
 
 class ScanSelector:
-    def __init__(self, window, scans):
+    def __init__(self, window, io_box):
         self.window = window
-        self.scans = scans
-        self.options = scans.keys()
+        self.io_box = io_box
+        self.scans = {"Google & Cloudflare DNS":""}
+        self.options = self.scans.keys()
         self.selection = StringVar()
-        self.dropdown = OptionMenu(self.window, self.selection, command=self.update, *self.options)
+        self.dropdown = self.make_dropdown()
         self.delete_button = Button(self.window, text="X", command=self.delete)
+        self.mount_dropdown()
+        self.mount_delete_button()
 
-    def mount(self):
+    def mount_dropdown(self):
         self.dropdown.place(x=355, y=150, width=200, height=50)
+
+    def mount_delete_button(self):
         self.delete_button.place(x=555, y=150, width=35, height=50)
 
     def delete(self):
-        self.options.pop(self.selection.get())
         self.scans.pop(self.selection.get())
+        self.update_options()
 
-    def update(self):
-        pass
+    def add_scan(self, scan_name, scan_output):
+        self.scans[scan_name] = scan_output
+        self.update_options()
+
+    def update_frame(self):
+        return self.scans[self.selection.get()]
+
+    def make_dropdown(self):
+        return OptionMenu(self.window, self.selection, command=self.update_frame, *self.options)
+
+    def update_options(self):
+        self.options = self.scans.keys()
+        self.dropdown.destroy()
+        self.dropdown = self.make_dropdown()
+        self.mount_dropdown()
